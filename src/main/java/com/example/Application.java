cf98fc845991ddae16f6964f0b0013ab623faae8
Gafari Salaudeen <gafarioladele1@gmail.com>
2026-05-31 21:47:56 -0700

    feat: add startup parameter logging

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -14,6 +14,14 @@ public class Application {
     {
         SpringApplication.run(Application.class, args);
         // add your code snippet here
+        Logger log = LoggerFactory.getLogger(Application.class);
+        try {
+            String one = args[0];
+            String two = args[1];
+            log.info("Application will start with the parameters {} and {}", one, two);
+        } catch (Exception e) {
+            log.info("No parameters provided");
+        }
     }
 
     @PostConstruct
